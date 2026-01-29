2d8fe0fc87d8d57054b70635d4a9d39dd5bf858b
sagitta42 <mariia.redchuk@gmail.com>
2026-01-29 22:46:20 +0100

    update examples and main

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+import sys
+
 from src.lyrics_generator import generate_lyrics
 
-# lyrics_path = "examples/all_albums.csv"
-lyrics_path = "examples/into_glory_ride.csv"
+lyrics_path = sys.argv[1]
+settings_path = sys.argv[2]
 
-generate_lyrics(lyrics_path, "examples/settings_original.json")
+generate_lyrics(
+    lyrics_path=lyrics_path, settings_path=settings_path, output_type="save_txt"
+)
